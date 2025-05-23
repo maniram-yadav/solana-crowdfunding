bfa61887ae627feef458f5e7f437845abc676b5c
maniram <maniramyadav786@gmail.com>
2025-05-23 13:12:15 +0000

    ts config modied

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,29 @@
 'use client'
 
+import CampaignCard from '@/components/CampaignCard'
+import CampaignHero from '@/components/CampaignHero'
 import { useEffect, useMemo, useState } from 'react'
+import {
+  fetchActiveCampaigns,
+  getProviderReadonly,
+} from '@/services/blockapi'
+import { Campaign } from '@/utils/interfaces'
+
 
 export default function Page() {
+
+  const [campaign, setCampaign] = useState<Campaign[]>([])
+  const program = useMemo(() => getProviderReadonly(), [])
+
+  useEffect(() => {
+    fetchActiveCampaigns(program!).then((data) => setCampaign(data))
+  }, [program])
 
 
   return (
     <div className="container mx-auto p-6">
-        main body
+      <CampaignHero />
+      main body
     </div>
   )
 }
--- src/app/services/blockapi.tsx
+++
@@ -1,7 +0,0 @@
-import { AnchorProvider,BN,Program,Wallet } from "@coral-xyz/anchor";
-import { Connection,PublicKey,SystemProgram,TransactionSignature } from "@solana/web3.js";
-import { Crowdfunding } from "../../../anchor/target/types/crowdfunding";
-import  idl from "../../../anchor/target/idl/crowdfunding.json";
-// import {  } from "@/utils/interfaces/";
-// import {  } from "module";
-// import {  } from "module";
