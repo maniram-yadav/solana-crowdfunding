2403df542e8a32987b8cd2148e82950befbdf564
maniram <maniramyadav786@gmail.com>
2025-05-23 10:44:53 +0000

    cusomt data added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,18 @@
-import type {Config} from "tailwindcss";
-
-const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+module.exports = {
   theme: {
-    extend: {},
-  },
-  plugins: [require('daisyui')],
-};
-export default config;
+    extend: {
+      transformStyle: {
+        'preserve-3d': 'preserve-3d',
+      },
+      backfaceVisibility: {
+        hidden: 'hidden',
+      },
+      perspective: {
+        '1000': '1000px',
+      },
+      rotate: {
+        'y-180': 'rotateY(180deg)',
+      },
+    }
+  }
+}
